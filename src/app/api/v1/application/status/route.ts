288eb0326eb7aaf985560ed256174ee6749bf0c6
Sudeep Dutta <duttasudeep105@gmail.com>
2026-03-14 20:13:02 +0530

    changes

--- src/app/api/v1/application/status/route.ts
+++ src/app/api/v1/application/status/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 const corsHeaders = {
-    "Access-Control-Allow-Origin": "https://puratanayurveda.com",
+    "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
